6853a39447e14a5111eddcf4cef877b46513f8ff
Zach Webb <webbufcs@gmail.com>
2026-01-17 19:14:39 -0500

    more

---
+++ EricBigAdventure/Assets/Script/World Characters/VotingBooth.cs
@@ -0,0 +1,49 @@
+using NUnit.Framework;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VotingBooth : MonoBehaviour
+{
+    private Interactable _interactable;
+
+    [SerializeField] List<Dialogue.DialogueEntry> _Dialogues;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        _interactable = GetComponentInChildren<Interactable>();
+
+        if (StoryManager.Instance.GetProgress("Pen") > 0)
+        {
+            _interactable.interactText = "Perform Civic Duty";
+        }
+
+        if (StoryManager.Instance.GetProgress("Voting") > 1)
+        {
+            gameObject.SetActive(false);
+        }
+    }
+
+    public void Penhaviour()
+    {
+        if (StoryManager.Instance.GetProgress("Pen") > 0)
+        {
+            _Dialogues[1].Play();
+        }
+        else
+        {
+            _Dialogues[0].Play();
+        }
+    }
+
+    public void NoProgress()
+    {
+        _interactable.Reenable();
+    }
+
+    public void Vote()
+    {
+        //TODO: Load into voting scene
+        //StoryManager.Instance.SetProgress("Pen", 1);
+        //TODO: Danny works his magic
+    }
+}
